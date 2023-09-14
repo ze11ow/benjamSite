078a5abef205fcec71c2cde144f99841dc2651cd
Ben <63691941+ze11ow@users.noreply.github.com>
2023-09-14 16:56:46 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,15 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+let index = 0;
+displayImages();
+function displayImages() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  index++;
+  if (index > images.length) {
+    index = 1;
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
 }
